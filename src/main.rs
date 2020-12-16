d5f71dcdef9b356b016c14834437fb9f78dcf277
kyssion <1409915687@qq.com>
2020-12-16 21:00:18 +0800

    day : 2020-12-16

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod rust0001_base_example;
 mod rust0000_guessing_game;
 
 fn main() {
-    // rust0000_guessing_game::test1();
-    rust0001_base_example::test1()
+    rust0000_guessing_game::test1();
+
 }
