2fa8d2dda02b785e581f236d87a81793bfd58553
kyssion <1409915687@qq.com>
2020-12-17 20:54:53 +0800

    day: 2020-12-17

--- src/rust0001_base_example.rs
+++ src/rust0001_base_example.rs
@@ -1,3 +1,5 @@
+use std::arch::mips::break_;
+
 pub fn test1() {
     //let 和mut let mut 类型可以进行定义并且进行相关的修改
     let value1 = 10;
@@ -15,7 +17,7 @@ pub fn test1() {
 const MAX_LENGTH: i32 = 1000;
 
 pub fn data_type() {
-    //整形
+    //todo 整形
     let i: i8;
     let i: i16;
     let i: i32;
@@ -25,26 +27,26 @@ pub fn data_type() {
 
     // 整型溢出 - debug模式抛出异常 ， 发布模式 补码环绕
 
-    //浮点型
+    //todo 浮点型
     let f:f32;
     let f:f64;
 
-    //bool
+    //todo bool
     let b:bool = false;
     let b : bool = true;
 
-    //字符类型 - rust 字符使用的是Unicode编码 占用4 个字节
+    //todo 字符类型 - rust 字符使用的是Unicode编码 占用4 个字节
     let c : char = '总';
     
-    //复合类型
+    // todo 复合类型
     
-    //元组
+    // todo 元组
     let y :(i32,f32,u8) = (12,12.0,1);
     let (a,b,_) = y; // rust 这里和golang的处理方法类似使用_ 来丢弃不用的值
 
     let c = y.1;// 元组可以使用的后缀+ 数字来确定标记位置
 
-    //数组
+    // todo 数组
     let a = [1,2,3,4,5,6]; // 数组初始化
     let b : [i32; 10]; // 初始化声明数组
 }
@@ -65,12 +67,57 @@ pub fn statement_and_expression(){
     let i = if(i>3){
         12
     }else{
-        13
+        13 // todo 注意使用这种特性的时候要保证 各个判断分支返回的表达式类型时相同的
+    };
+
+
+    //loop
+    let item = loop{
+        break 1+123; // todo 注意如果返回值比较多的时候要保证类型是相同的
     };
 }
 
-//注意rust 使用蛇形命名方法 ， 参数和基本明明方法相同，返回值如果有多个的时候使用元组
+// todo 注意rust 使用蛇形命名方法 ， 参数和基本明明方法相同，返回值如果有多个的时候使用元组
 pub fn func_test(i : i32)-> String{
     return "123123123".parse().unwrap();
+}
+
+// 控制流
+
+pub fn process_test(){
+    //todo  if else
+    let number = 3;
+    // 基本的流程语句结构
+    if number <5 {// rust 表达式计算之后的结果必定为true或者false
+
+    }else if number>=5 && number<10{
+
+    }else{
+
+    }
+
+    // 使用控制流程语法规则 - 利用 表达式语句特性
+    let a = if number<5{
+        5
+    }else{
+        10 // todo 注意使用这种特性的时候要保证 各个判断分支返回的值类型时相同的
+    };
+
+
+    // todo 循环结构 rust中有三种类型 loop while  for
+
+    //  loop 死循环
+    loop{
+        println!("loop");
+        break;
+    }
+
+    //todo 表达式特性 - 和 if 相同 要保证返回值类型一致 使用 break+返回值抛出
+
+    let item = loop{
+        break 1+123;
+    };
+
+
 }
 
