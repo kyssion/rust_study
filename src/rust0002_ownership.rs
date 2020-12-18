69b57541cba6cd499753dc5de7de76d67acade07
kyssion <1409915687@qq.com>
2020-12-18 12:19:55 +0800

    day: 2020-12-18

--- src/rust0002_ownership.rs
+++ src/rust0002_ownership.rs
@@ -10,5 +10,14 @@ pub fn ownership_main(){
     //todo 和 &str 相比，&str 不可变
     let mut str = "str item";
 
+    //todo 克隆和转移 - 基本的变量进行赋值的时候的默认使用的方法是clone
+    //todo 控制这两个的方法是 Copy和Drop的trait
+    let i = 5;
+    let j = i;
+    // --- --- ---
+    let i = String::from("owner string");
+    let j = i;// todo 这个时候的i 是不可使用的 i 的所有权已经转移到了j 上面了
+    let v = j.clone();//使用clone 可以获取一个string字符串的clone
+    // println!(i);
 
 }
