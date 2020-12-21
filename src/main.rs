422921af45369bdbb03e8eac8ed31aa2df91aad4
kyssion <1409915687@qq.com>
2020-12-21 20:59:44 +0800

    feat: 2020-12-21

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 mod rust0001_var_and_control_flow;
 mod rust0000_official_example;
 mod rust0002_ownership;
+mod rust0003_struct;
 
 fn main() {
     let mut string = String::from("test item");
---
+++ src/rust0003_struct.rs
@@ -0,0 +1,9 @@
+
+
+//定义一个rust结构体
+struct User{
+    user_name:String,
+    email:String,
+    sign_in_count: u64,
+    active: bool
+}
