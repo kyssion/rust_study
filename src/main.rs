ab5d32da1af6d94a0bae70692f1da3aaa01da66c
kyssion <1409915687@qq.com>
2021-07-05 16:15:42 +0800

    feat: 2021-07-05

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use crate::rust05_trait_lifetione_types::Summary;
+
 mod rust00_base;
 mod rust01_ownership;
 mod rust02_struct_enum;
@@ -5,7 +7,31 @@ mod rust03_collection_string_map;
 mod rust04_panic;
 mod rust05_trait_lifetione_types;
 
-
 fn main(){
+    let ppp = P{};
+    ppp.summarize();
+    let a = String::from("fff");
+    println!("{}",a.summarize());
+}
 
+struct P{}
+
+impl Summary for P{
+    fn summarize(&self) -> String {
+        String::from("zzz")
+    }
+
+    fn prn() -> String {
+        return String::from("P")
+    }
+}
+
+impl Summary for String{
+    fn summarize(&self) -> String {
+        return String::from(self);
+    }
+
+    fn prn() -> String {
+        return String::from("z")
+    }
 }
--- src/rust05_trait_lifetione_types.rs
+++ src/rust05_trait_lifetione_types.rs
@@ -17,11 +17,90 @@ struct Point<T> {
     y: T,
 }
 
-fn structType(){
+//2.1 rust 针对struct 定义的方法 ， 可以做到指定类型的存在各自参数的能力
+impl<T> Point<T>{
+    fn show(&self)->&T{
+        return &self.x
+    }
+}
+
+impl Point<i32>{
+    fn show_i32(&self)->i32{
+        return self.x
+    }
+}
+
+fn struct_type(){
     let a = Point{
         x:1,y:2
     };
     let b= Point{
         x:"fff",y:"xxxx"
     };
+}
+//3. 枚举类型也支持范型
+
+enum Test<T>{
+    One(T),
+    None
+}
+
+fn enum_type(){
+    let a = Test::One(String::from("ffff"));
+}
+
+//1. rust traits - 类似其他编程语言的接口概念， 但是还是有一些区别
+// rust 中的traits 其实实现了 定义一组类型的通用能力
+
+pub trait Summary {
+    //存在self 表示可以使用. 的方法调用
+    fn summarize(&self) -> String;
+    //prn 表示可以使用Struct 名称+:: 的方法调用
+    fn prn()->String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+
+    fn prn() -> String {
+        return String::from("prn new article")
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+    fn prn() -> String {
+        return String::from("prn tweet")
+    }
+}
+
+pub fn test(){
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from(
+            "of course, as you probably already know, people",
+        ),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
