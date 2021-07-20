7d5fcbac91b2ee6b4da12e3c44ea509a64938d95
kyssion <1409915687@qq.com>
2021-07-20 15:21:21 +0800

    feat: 2021-07-20

--- src/rust06_functional_features.rs
+++ src/rust06_functional_features.rs
@@ -9,7 +9,7 @@ pub fn test<T>(i:T){
     let c= ppp;
     c(111);
     //3. 内部闭包其实可以使用类型推单来避免参数声明
-    let test_fn =|num|{
+    let test_fn = |num|{
         return num;
     };
     let b = test_fn(123);
@@ -18,8 +18,8 @@ pub fn test<T>(i:T){
 
     //4. 闭包获取上下文环境
     let num_info = 1233;
-    let fn_test = ||{
-        println!("{}",num_info)
+    let fn_test = || {
+        println!("{}",num_info);
     };
     //5. 闭包和函数的三种实现trait
     //5.1 FnOnce todo 暂时不知道干啥的 感觉是使用move标记的变量信息
@@ -46,5 +46,5 @@ pub fn test<T>(i:T){
 }
 
 fn ppp<'a,T>(i : T){
-    println!("dfsdfsf")
+    println!("dfsdfsf");
 }
